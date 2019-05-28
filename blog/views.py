002defb78d999e51ee19b1cb7e1c630b1fec9ccb
shiva kumar chinnam <s_chinnam@apple.com>
2019-05-28 01:57:07 -0700

    End of video 5 - DB

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post 
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'Blog 1 content',
-        'date_posted': 'May 31 2019'
-    },
-    {
-        'author': 'Chinnam',
-        'title': 'Blog Post 2',
-        'content': 'Blog post 2 content',
-        'date_posted': 'June 1 2019'
-    }
-
-]
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Title'
     }
     return render(request, 'blog/home.html', context)
