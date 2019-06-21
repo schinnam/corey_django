c769e8b119218782a7c2ec86c32280cd6fc35ff5
shiva kumar chinnam <s_chinnam@apple.com>
2019-06-20 17:29:44 -0700

    added static folder, executed: python manage.py collectstatic

--- antinationalist/settings.py
+++ antinationalist/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
